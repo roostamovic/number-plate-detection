f875b637842e8cefed756850c9c583da0c6d6852
Doston Roostamovic <roostamovic@yahoo.com>
2022-01-13 10:50:27 +0500

    Update number_plate_detection.py

--- number_plate_detection.py
+++ number_plate_detection.py
@@ -1,8 +1,4 @@
 import cv2
-import pytesseract # This is the TesseractOCR Python library
-import matplotlib.pyplot as plt
-# Set Tesseract CMD path to the location of tesseract.exe file
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 frameWidth = 640
 frameHeight = 480
@@ -18,32 +14,22 @@ cap.set(4, frameHeight)   # 4 is ID of height
 cap.set(10, 150)  # 10 is ID of brightness
 
 
-
 while True:
     success, img = cap.read()
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     numberPlates = plateCascade.detectMultiScale(imgGray, 1.1, 10)
-    
     for (x, y, w, h) in numberPlates:
-        
         area = w*h
         if area > minArea:
             cv2.rectangle(img, (x,y), (x+w, y+h), color, thickness=3)
-            cv2.putText(img, text, (x, y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, color, 2)
+            cv2.putText(img, text, (x, y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (250,250,250), 2)
             imgRoi = img[y:y+h, x:x+w]
             cv2.imshow('RoI', imgRoi)
             
             cv2.imwrite('Resources/Scanned/Number_Plate_'+str(count)+'.jpg', imgRoi)
             count += 1
-            
-    
-
     img = cv2.resize(img, (1000, 640))
     cv2.imshow('RESULT', img)
 
-    if cv2.waitKey(1) & 0xFF == ord('s'):
-        #cv2.rectangle(img, (0,200), (640,300), (0,255,0), cv2.FILLED)
-        #cv2.putText(img, 'SAVED', (150,265), cv2.FONT_HERSHEY_DUPLEX, 2, (0,0,255), 2)
-        #cv2.imshow('RESULT', img)
-        break
-        
+    if cv2.waitKey(1) & 0xFF == ord('s'): # if 'Q' is pressed, the program will stop
+        break    
